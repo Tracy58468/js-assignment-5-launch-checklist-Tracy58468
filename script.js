209d2097461612dd2840e3f802ac2a3b501f4f88
Tracy H <aag9n17o@duck.com>
2023-07-29 14:54:54 -0500

    Finally got json to log to console.

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // Write your JavaScript code here!
 
+// window.addEventListener("load", function() {
+//     const form = document.getElementById("launchForm");
+//     // const launchForm = document.getElementById("launchForm");
+//     const pilotName = document.getElementById("pilotName");
+//     const copilotName = document.getElementById('[name="copilotName"]');
+//     const fuelLevel = document.getElementById('[name="fuelLevel"]');
+//     const cargoMass = document.getElementById('[name="cargoMass"]');
+//     const faultyItems = document.getElementById('faultyItems');
+
+//     form.addEventListener("submit", function(event) {
+//         let pilotNameInput = document.querySelector("input[name = pilotName]");
+//         // console.log(pilotNameInput.value);
+//         let copilotNameInput = document.querySelector("input[name = copilotName]");
+//         let fuelLevelInput = document.querySelector("input[name = fuelLevel]");
+//         let cargoMassInput = document.querySelector("input[name = cargoMass]");
+//         let list = faultyItems;
+//         let launchStatus = document.getElementById('launchStatus');
+
+//         formSubmission(document, list, pilotNameInput.value, copilotNameInput.value, fuelLevelInput.value, cargoMassInput.value);
+
+//         event.preventDefault();
+//     });      
+// });
+
 window.addEventListener("load", function() {
+
     const form = document.getElementById("launchForm");
     // const launchForm = document.getElementById("launchForm");
     const pilotName = document.getElementById("pilotName");
@@ -11,7 +36,7 @@ window.addEventListener("load", function() {
 
     form.addEventListener("submit", function(event) {
         let pilotNameInput = document.querySelector("input[name = pilotName]");
-        console.log(pilotNameInput.value);
+        // console.log(pilotNameInput.value);
         let copilotNameInput = document.querySelector("input[name = copilotName]");
         let fuelLevelInput = document.querySelector("input[name = fuelLevel]");
         let cargoMassInput = document.querySelector("input[name = cargoMass]");
@@ -21,22 +46,24 @@ window.addEventListener("load", function() {
         formSubmission(document, list, pilotNameInput.value, copilotNameInput.value, fuelLevelInput.value, cargoMassInput.value);
 
         event.preventDefault();
-    });      
-});
+    });
 
-// window.addEventListener("load", function() {
+    myFetch();
 
-//    let listedPlanets;
-//    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-//    let listedPlanetsResponse;
-//    listedPlanetsResponse.then(function (result) {
+//     let listedPlanets;
+//     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+//     let listedPlanetsResponse = planetsReturned;
+//     listedPlanetsResponse.then(function (result) {
 //        listedPlanets = result;
 //        console.log(listedPlanets);
-//    }).then(function () {
+//     }).then(function () {
 //        console.log(listedPlanets);
 //        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       
+//     //    pickPlanet(listedPlanetsResponse);
+//     //    addDestinationInfo(document, pickedPlanet.name, pickedPlanet.diameter, pickedPlanet.star, pickedPlanet.distance, pickedPlanet.moons, pickedPlanet.imageUrl);
 //    })
    
-// });
+});
 
 /*ONLY MODIFY THIS AND SCRIPTHELPER.JS*/
--- scriptHelper.js
+++ scriptHelper.js
@@ -16,6 +16,18 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
     const missionTarget = document.getElementById("missionTarget");
 
+    missionTarget.innerHTML = `
+        h2>Mission Destination</h2>
+            <ol>
+                <li>Name: ${name}</li>
+                <li>Diameter: ${diameter}</li>
+                <li>Star: ${star}</li>
+                <li>Distance from Earth: ${distance}</li>
+                <li>Number of Moons: ${moons}</li>
+            </ol>
+        <img src="${imageUrl}">
+    `
+
 }
 
 function validateInput(testInput) {
@@ -144,17 +156,23 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
         response.json().then(function(json) {
-            div.
+            console.log(json);
+            return json;
         })
-        });
+    });
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
-    let pickedPlanet = Math.floor(Math.random()*(planets.length + 1));
+    let pickedPlanetNum = Math.floor(Math.random()*(planets.length + 1));
+    console.log(`pickedPlanetNum:  ${pickedPlanetNum}`);
+    // now get planet name
+    let pickedPlanet = planets.pickedPlanetNum;
+    console.log(`pickedPlanet:  ${pickedPlanet}`);
+    return pickedPlanet;
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
@@ -164,14 +182,3 @@ module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
 
 /*ONLY MODIFY THIS AND SCRIPT.JS*/
-
-/*
-Adding Validation
-Adding Alerts
-
- In scriptHelper.js, you will use validateInput() to complete the formSubmission() function. formSubmission() will take in a document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass. Using the values in those strings and the document parameter for your HTML document, update the shuttle requirements as described below. Make sure to call your formSubmission() function at the appropriate time in your script.js file!
-
-Note
-
-If you want to check if something is NaN, you cannot use == or ===. Instead, JavaScript has a built-in method called isNaN(value) that returns true if value is NaN and false if value is not NaN.
-*/
